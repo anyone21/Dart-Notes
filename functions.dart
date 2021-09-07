847e3780c0d72d5341c4144c2d1bcd0f398132b6
annoying_code <32765126+anyone21@users.noreply.github.com>
2021-09-07 18:17:26 +0530

    Create functions.dart

---
+++ functions.dart
@@ -0,0 +1,22 @@
+// Mini-ex1
+
+String youAreWonderful({String name, int numberPeople=30}) {
+  return 'You\'re wondeful, $name. $numberPeople people think so.';
+}
+
+
+
+// Mini- ex2
+
+void main() {
+  final wonderful = (String name) {
+    return 'You\'re wonderful, $name.';
+  };
+
+  const people = ['Chris', 'Tiffni', 'Rohan', 'Rakesh'];
+
+  people.forEach((p) {
+    print(wonderful(p));
+  });
+}
+
