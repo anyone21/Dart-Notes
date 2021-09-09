1289ab7f3a5dde0783643c80dfcf45bf05a0ec0d
annoying_code <32765126+anyone21@users.noreply.github.com>
2021-09-09 17:05:23 +0530

    Create Chapter4.dart

---
+++ Chapter4.dart
@@ -0,0 +1,17 @@
+// Mini-exercise 1
+void main(List<String> args) {
+  const myAge = 24;
+  const isTeenager = (myAge >= 13 && myAge <= 19);
+  // print(isTeenager);
+
+  const maryAge = 30;
+  const isTeenMary = (maryAge >= 13 && maryAge <= 19);
+  const bothTeenagers = isTeenMary && isTeenMary;
+
+  const String reader = 'ujjwal';
+  const String ray = 'Ray Wenderlich';
+  const bool rayIsReader = reader == ray;
+  print(rayIsReader);
+}
+
+
