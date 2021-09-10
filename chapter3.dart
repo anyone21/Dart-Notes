e41f58a27fd3d5bd15d5ca43f9373a4504625ab3
annoying_code <32765126+anyone21@users.noreply.github.com>
2021-09-10 22:42:35 +0530

    Create chapter3.dart

---
+++ chapter3.dart
@@ -0,0 +1,11 @@
+// Mini-Exercise1
+void main(List<String> args) {
+  const age1 = 42; //int
+  const age2 = 21; //int
+
+  const averageAge = (age1 + age2) / 2;  //double
+  // averageAge is double because type get infer at runtime
+
+}
+
+
