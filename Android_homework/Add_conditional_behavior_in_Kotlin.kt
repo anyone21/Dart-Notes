5a55c52e7452f4f001669dd2b5fc077edb90dcce
annoying_code <32765126+anyone21@users.noreply.github.com>
2021-11-05 13:26:08 +0530

    Create Add_conditional_behavior_in_Kotlin.kt

---
+++ Android_homework/Add_conditional_behavior_in_Kotlin.kt
@@ -0,0 +1,23 @@
+fun main() {
+    val myFirstDice = Dice(8)
+    val rollResult = myFirstDice.roll()
+    val luckyNumber = 4
+
+    when (rollResult) {
+        luckyNumber -> println("You won!")
+        1 -> println("So sorry! You rolled a 1. Try again!")
+        2 -> println("Sadly, you rolled a 2. Try again!")
+        3 -> println("Unfortunately, you rolled a 3. Try again!")
+        5 -> println("Don't cry! You rolled a 5. Try again!")
+        6 -> println("Apologies! you rolled a 6. Try again!")
+        7 -> println("Apologies! you rolled a 7. Try again!")
+        8 -> println("Apologies! you rolled a 8. Try again!")
+        
+   }
+}
+
+class Dice(val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
